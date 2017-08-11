f466f0ede196fdc3ee2efed1745eff3195d751c7
tianlman <tianlman@163.com>
2017-08-11 18:58:30 +0800

    。。。

--- src/router/index.js
+++ src/router/index.js
@@ -1,37 +1,29 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Hello from '@/components/Hello'
-import test from '@/components/test'
-import test2 from '@/components/test2'
-import login from '@/views/login'
-import left from '@/views/left'
+import Vue from "vue";
+import Router from "vue-router";
+import Hello from "@/components/Hello";
+import test from "@/components/test";
+import test2 from "@/components/test2";
+import login from "@/views/login";
 
-Vue.use(Router)
+Vue.use(Router);
 const routes= [
   {
-    path: '/',
-    name: 'Hello',
-    component: Hello
-  },
-  {
-    path: '/test',
-    name: 'test',
+    path: "/test",
+    name: "test",
     component: test
   },{
-    path: '/test2',
-    name: 'test2',
+    name: "Hello",
+    path: "/",
+    component: Hello
+  },{
+    path: "/test2",
+    name: "test2",
     component: test2
   },{
-    path: '/login',
-    name: 'login',
+    path: "/login",
+    name: "login",
     component: login
-  },{
-    path: '/left',
-    name: 'left',
-    component: left
-  }
-
-]
+  }];
 
 export default new Router({
   routes
