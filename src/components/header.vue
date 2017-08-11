2f55f9c1970f2f339cec1f7b9f56c015fa26ea54
evenrsh <15813307192@163.com>
2017-08-11 15:45:11 +0800

    文件配置

---
+++ src/components/header.vue
@@ -0,0 +1,20 @@
+<template lang="html">
+    <div>
+
+    </div>
+</template>
+
+<script lang="babel">
+    export default {
+        name: '',
+        data() {
+            return {}
+        },
+        components: {}
+
+    }
+</script>
+
+<style lang="scss">
+
+</style>
