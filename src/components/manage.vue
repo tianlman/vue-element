c934ed62a0dfdc3712f702a2fbc0628dbadcd8ea
tianlman <tianlman@163.com>
2017-08-14 15:58:23 +0800

    配置路由

--- src/components/manage.vue
+++ src/components/manage.vue
@@ -1,17 +1,7 @@
 
 <template>
   <div>
-    <el-button>默认按钮</el-button>
-    <!--<el-button type="primary">主要按钮</el-button>-->
-    <!--<el-button type="text">文字按钮</el-button>-->
-    <!--<my-component></my-component>-->
-    <my-component></my-component>
-    <p>test{{$store.state.name}}</p>
-    <span class="span" @click = a>button</span>
-    <span class="span" @click = get>ajax</span>
-    <input type="text" v-model="test">
-    <span @click="setText2">aaa</span>
-    <div class="aa"><span class="bb">{{getTest}}</span></div>
+
   </div>
 </template>
 
